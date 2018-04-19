fa59ad168ba41caaa69fd54d7d8daa1a439db4a8
Yuan Lei <yuanlei@fb.com>
2018-04-19 12:25:35 +0100

    refine

--- Misc/vim_setup.sh
+++ Misc/vim_setup.sh
@@ -1,6 +1,9 @@
 cd ~
 git clone https://github.com/vim/vim.git Vim
 
+#fix ycm build core error: python config header file not found
+sudo apt-get install python-dev
+
 ./configure --enable-pythoninterp=yes
 make
 make install
